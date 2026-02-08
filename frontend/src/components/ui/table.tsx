03d8e680dbc5d177aa910fb9522d8c2234a261bd
AlishaaShahid <bsdsf22a005@pucit.edu.pk>
2026-02-08 09:10:42 +0500

    Fix Vercel build: lib/utils, Tailwind purge, UI imports from ../../lib/utils

--- frontend/src/components/ui/table.tsx
+++ frontend/src/components/ui/table.tsx
@@ -1,7 +1,7 @@
 
 import * as React from "react"
 
-import { cn } from "../../utils"
+import { cn } from "../../lib/utils"
 
 function Table({ className, ...props }: React.ComponentProps<"table">) {
   return (
