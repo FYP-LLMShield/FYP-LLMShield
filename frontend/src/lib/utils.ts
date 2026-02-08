01ccc0f58510f6ac1fc505099e70759725e214dd
AlishaaShahid <bsdsf22a005@pucit.edu.pk>
2026-02-08 09:10:42 +0500

    Fix Vercel build: lib/utils, Tailwind purge, UI imports from ../../lib/utils

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,1 +1,10 @@
-export { cn } from "../utils";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Combines clsx and tailwind-merge for conditional class names.
+ * Used by UI components in src/components/ui/.
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
